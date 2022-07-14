7e3d7bf7c7038b670cbd02abab9f70488f813d0d
Naman <namandalsania12@gmail.com>
2022-07-14 19:30:50 +0530

    First Commit

---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+
+# Create your views here.
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        if username and password:
+            user = auth.authenticate(request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, "Login Success")
+                return redirect("/")
+            messages.error(request, "Incorrect Credentials")
+            return render(request, "login.html", {"username": username})
+            
+    return render(request, "login.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        email = request.POST.get("email", None)
+        
+        try:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            auth.login(request, user)
+        except:
+            messages.error(request, "User with this username already exists!!")
+            return render(request, "register.html", {"username" : username, "email" : email})
+        
+        messages.success(request, "User created successfully!!")
+        return redirect("/")
+    return render(request, "register.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
+
---
+++ services/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import Url
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+import random, string
+
+def getAlias():
+    return "".join([random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(8)])
+
+# Create your views here.
+def dashboard(request):
+    if request.method == "POST":
+        URL = request.POST["URL"]
+        alias = request.POST.get("alias", None)
+        
+        if not alias:
+            alias = getAlias()
+        
+        try:
+            Url.objects.create(user=request.user, target_url=URL, alias=alias).save()
+            messages.success(request, "Shorted Successfully.")
+            return redirect("dashboard")
+        except:
+            messages.error(request, "Alias already in use.")
+            return render(request, "dashboard.html", {"url": URL, "alias": alias})
+        
+    site = get_current_site(request)
+    return render(request, "dashboard.html", {"domain": site})
+
+def redirect_to_target_page(request, alias):
+    obj = Url.objects.get(alias=alias)
+    URL = obj.target_url
+    return redirect(URL)
+
